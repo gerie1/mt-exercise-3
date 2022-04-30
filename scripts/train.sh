663253bee18b5b25772a580f0013a8bac0cafe8b
Kim Hai Dinh <45572980+gerie1@users.noreply.github.com>
2022-05-01 00:41:15 +0200

    Update train.sh

--- scripts/train.sh
+++ scripts/train.sh
@@ -18,8 +18,8 @@ SECONDS=0
     CUDA_VISIBLE_DEVICES=$device OMP_NUM_THREADS=$num_threads python main.py --data $data/inaugural_speeches \
         --epochs 40 \
         --log-interval 100 \
-        --emsize 250 --nhid 250 --dropout 0.7 --tied \
-        --save $models/model07.pt
+        --emsize 250 --nhid 250 --dropout 0.2 --tied \
+        --save $models/model02.pt
 )
 
 echo "time taken:"
